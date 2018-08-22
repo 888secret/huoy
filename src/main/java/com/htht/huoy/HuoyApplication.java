269a20c24f44776f06f2c2077de848c1d3ca2718
wjx <694110206@qq.com>
2018-08-22 18:20:47 +0800

    22

---
+++ src/main/java/com/htht/huoy/HuoyApplication.java
@@ -0,0 +1,12 @@
+package com.htht.huoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuoyApplication.class, args);
+    }
+}
